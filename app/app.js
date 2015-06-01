59812a8d9ba06009fa42a892dc0d0a0e4a2ef549
Mitch Lloyd <mitch.lloyd@gmail.com>
2015-05-31 21:06:01 -0400

    Render something with crazy fast performance!

--- app/app.js
+++ app/app.js
@@ -1,4 +1,7 @@
+import renderSomething from 'no-ember/modules/render-something';
+
 export default {
   create() {
+    renderSomething();
   }
 };
---
+++ app/modules/render-something.js
@@ -0,0 +1,3 @@
+export default function renderSomething() {
+  document.write("Who needs a framework?");
+}
